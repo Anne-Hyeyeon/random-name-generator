294a259b5ea5ce548b1a09d3e0125d51bb8fdb80
Hyeyeon(Anne) <khy2106@naver.com>
2022-09-29 00:52:06 +0900

    새로고침시 undefined

--- src/Name.js
+++ src/Name.js
@@ -2,24 +2,24 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 function Name() {
-    const SERVER_URL = "https://api.parser.name/?api_key=563dbb8df16922a5622e5f2a1a3a84e2&endpoint=generate&country_code=DE";
-    const [nameData, setNameData] = useState([]);
-    const getNameData = async () => { 
-        try {  
-          const res = await axios.get(SERVER_URL);
-          setNameData(res.data.data[0]);
-          console.log(res.data.data[0].name);
-        } catch(err) {
-          console.log(err);
-        }
-    }
+    const SERVER_URL = "https://api.parser.name/?api_key=699d0e0711f7c175edbe93d887767f1a&endpoint=generate&country_code=US";
     useEffect(() => {
+        const getNameData = async () => { 
+            try {  
+              const res = await axios.get(SERVER_URL);
+              setNameData(res.data.data[0].name);
+            } catch(err) {
+              console.log(err);
+            }
+        }
         getNameData();
-    }, [])
-    
+    },[])
+    const [nameData, setNameData] = useState([]);
+    console.log(nameData.firstname.name)
+
     return (
         <div>
-          hi
+            
         </div>
     );
 }
